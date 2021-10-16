5c88480c5ed49329a2960874c528684bf22aaade
Maki Aboabida <maki-miko@hotmail.com>
2021-10-16 00:05:01 -0400

    Added: category Routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,73 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require('express').Router()
+const { Category, Product } = require('../../models')
+const category404Message = 'No category found with this id'
 
 // The `/api/categories` endpoint
 
+// get all categories 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-});
+  Category.findAll({
+    attributes: ['id', 'category_name']
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => res.status(500).json(err))
+})
 
+// get one category 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      return res.status(404).json({message: category404Message})
+    }
+    return res.json(dbCategoryData)
+  })
+  .catch(err => res.status(500).json(err))
+})
 
+// add a category 
 router.post('/', (req, res) => {
-  // create a new category
-});
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => res.status(500).json(err))
+})
 
+// update a category 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      return res.status(404).json({message: category404Message})
+    }
+    return res.json(dbCategoryData)
+  })
+  .catch(err => res.status(500).json(err))
+})
 
+// delete a category 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      return res.status(404).json({message: category404Message})
+    }
+    return res.json(dbCategoryData)
+  })
+  .catch(err => res.status(500).json(err))
+})
 
-module.exports = router;
+module.exports = router
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,31 +1,52 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+const product404Message = 'No product found with this id'
+
 
 // The `/api/products` endpoint
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => res.status(500).json(err))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    include: [
+      {
+          model: Category,
+          attributes: ['id', 'category_name']
+      },
+      // {
+      //   model: Tag,
+      //   attributes: []
+      // }
+    ]
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      return res.status(404).json({message: product404Message})
+    }
+    return res.json(dbProductData)
+  })
+  .catch(err => res.status(500).json(err))
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stocks: req.body.stocks,
+    category_id: req.body.category_id
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
